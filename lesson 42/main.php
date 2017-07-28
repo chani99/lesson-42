3ee0b1b37027d07ea3213c7cccdcd24f1c2ad4da
chani <click0548450396@gmail.com>
2017-07-28 10:00:41 +0300

    updeat for hw

--- lesson 42/main.php
+++ lesson 42/main.php
@@ -19,7 +19,7 @@ if(isset($_POST['submit'])) {
     $insertNew = $DB->prepare("INSERT INTO ls42_contents(header, article) VALUES(:header, :article)");
     $insertNew->execute(array("header"=> $title, "article"=> $article));
                 echo "your article was added to database";
-                echo printAll($DB);
+                printAll($DB);
 
 }
 
@@ -27,10 +27,19 @@ function printAll($DB){
 $DB = $DB;
 $newArticle = $DB->prepare('SELECT * FROM ls42_contents');
 $newArticle->execute();
+            echo "<head>
+                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+                </head><body>";
 
 while ($row = $newArticle->fetch()) {
-            echo "<h2>" . $row['header'] . "</h2></br><p>" . $row['article'] . "</p></br>";
+            echo "<div class='jumbotron' style='width:650px; margin-left: 50px; padding: 30px'><h2>" . $row['header'] . "</h2></br><p>" . addRowsToHtmlContent($row['article']) . "</p></div></br>";
+        }
+echo "</body>";
+}
+
+
+         function addRowsToHtmlContent($content) {
+            return str_replace(".", ".<br>", $content);
         }
 
-}
 ?>
